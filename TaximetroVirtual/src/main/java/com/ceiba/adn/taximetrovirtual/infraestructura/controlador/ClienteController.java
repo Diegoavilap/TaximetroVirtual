8ccf5a32ff69632cf4a982bfa86094931e5edd92
Diego Avila <diegoavilapava@hotmail.com>
2019-12-27 03:52:11 -0500

    se elimina paquete dto y se corrige el servicio crearCliente usando el dto del paquete mapeador

--- TaximetroVirtual/src/main/java/com/ceiba/adn/taximetrovirtual/infraestructura/controlador/ClienteController.java
+++ TaximetroVirtual/src/main/java/com/ceiba/adn/taximetrovirtual/infraestructura/controlador/ClienteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ceiba.adn.taximetrovirtual.aplicacion.dto.ClienteDTO;
+import com.ceiba.adn.taximetrovirtual.aplicacion.mapeador.ClienteDTO;
 import com.ceiba.adn.taximetrovirtual.aplicacion.servicio.ServicioCrearCliente;
 import com.ceiba.adn.taximetrovirtual.infraestructura.adaptador.repositorio.entidad.ClienteEntidad;
 
