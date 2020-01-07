9b72c998107887d4e02455043668a65b0c6d79ae
Diego Avila <diegoavilapava@hotmail.com>
2020-01-07 15:14:35 -0500

    se agrega front

---
+++ TaximetroVirtual/src/main/java/com/ceiba/adn/taximetrovirtual/infraestructura/controlador/excepcion/ExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.ceiba.adn.taximetrovirtual.infraestructura.controlador.excepcion;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.ceiba.adn.taximetrovirtual.dominio.excepcion.ExcepcionClienteNoEncontrado;
+import com.ceiba.adn.taximetrovirtual.dominio.excepcion.ExcepcionFechaFinalAnteriorAFechaInicial;
+
+
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler{
+
+	@org.springframework.web.bind.annotation.ExceptionHandler(ExcepcionFechaFinalAnteriorAFechaInicial.class)
+	public void excepcionFechaFinalInvalida(HttpServletResponse response) throws IOException {
+		response.sendError(HttpStatus.PRECONDITION_FAILED.value());
+	}
+	
+	@org.springframework.web.bind.annotation.ExceptionHandler(ExcepcionClienteNoEncontrado.class)
+	public void excepcionClienteNoEncontrado(HttpServletResponse response) throws IOException {
+		response.sendError(HttpStatus.BAD_REQUEST.value());
+	}
+}
