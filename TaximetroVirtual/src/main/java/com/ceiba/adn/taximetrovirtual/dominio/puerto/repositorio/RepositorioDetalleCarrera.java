b2966e14dec8aaded59b59215104193b04498558
Diego Avila <diegoavilapava@hotmail.com>
2020-01-09 10:53:32 -0500

    se documentan las interfaces con comentarios javadoc

--- TaximetroVirtual/src/main/java/com/ceiba/adn/taximetrovirtual/dominio/puerto/repositorio/RepositorioDetalleCarrera.java
+++ TaximetroVirtual/src/main/java/com/ceiba/adn/taximetrovirtual/dominio/puerto/repositorio/RepositorioDetalleCarrera.java
@@ -4,9 +4,21 @@ import java.util.List;
 
 import com.ceiba.adn.taximetrovirtual.dominio.modelo.DetalleCarrera;
 
-public interface RepositorioDetalleCarrera{
-	
-	public DetalleCarrera crear (DetalleCarrera detalleCarrera);
-	
+public interface RepositorioDetalleCarrera {
+
+	/**
+	 * Metodo que permite la persistencia de un DetalleCarrera
+	 * 
+	 * @param detalleCarrera
+	 * @return
+	 */
+	public DetalleCarrera crear(DetalleCarrera detalleCarrera);
+
+	/**
+	 * Método que se encarga de traer los DetalleCarrera que se encuentran
+	 * persitidos
+	 * 
+	 * @return
+	 */
 	public List<DetalleCarrera> listar();
 }
